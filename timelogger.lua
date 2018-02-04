1b7058b04af349bd2a5c1b098eb96e8e157e608d
hakonw <hakonwar@gmail.com>
2018-02-04 01:17:04 +0100

    Update time logger function, add function to display total time

--- timelogger.lua
+++ timelogger.lua
@@ -1,24 +1,18 @@
 local totaltime = 0
+local lasttime = os.clock()
 local filename = ""
--- how often to add seconds
-local updatetime = 1
 -- savepath to logfile
 local savepath = os.getenv("APPDATA") .. "\\mpv\\time.txt"
 -- local savepath = "C:\Users\user\AppData\Roaming\mpv\time.txt"
 
 
--- pauses and starts timer
+-- adds time to totaltime
 function pause_manager(name, value)
     if value == true then
-        timer:stop()
+        totaltime = totaltime + os.clock() - lastttime
     else
-        timer:resume()
+        lastttime = os.clock()
     end
-end
-
--- update total second variable
-function update()
-    totaltime = totaltime + updatetime
 end
 
 -- get file name
@@ -26,19 +20,41 @@ function startup(event)
     filename = mp.get_property("path")
 end
 
+-- check if file problems
+function file_exists(file)
+    local f, err = io.open(file, "rb")
+    if f == nil then
+      msg.error("Error opening file, error:" .. err)
+      print(err)
+      return false
+    end
+    f:close()
+    return true
+end
+
 -- write to file
 function shutdown(event)
-    file, err = io.open(savepath, "a")
-    if file == nil then
-        --msg.error("Error opening file")
-        print(err)
-    else
+    if file_exists(savepath) then
+        file = io.open(savepath, "a")
         file:write(totaltime .. "s, " .. os.date("%c") .. ", " .. filename, "\n")
         file:close()
     end
 end
 
-timer = mp.add_periodic_timer(updatetime, update);
+-- gets total time
+function total_time()
+    if not file_exists then return nil end
+    
+    local total = 0
+    for line in io.lines(savepath)
+        local s1, s2 = string:match(line, "(.?)s,(.*)")
+        total = total + s1
+    end
+    -- TODO: Transform time in s to days, hours, min, sec
+    mp.osd_message("Total time used: " .. total)
+end
+
 mp.observe_property("pause", "bool", pause_manager)
 mp.register_event("file-loaded", startup)
 mp.register_event("shutdown", shutdown)
+mp.add_key_binding("k","total_watch_time", total_time)
